15a999b104d360227e98bc90a6baf9b962f8eb7a
eman hemida <hemidaeaman75@gmail.com>
2024-12-27 12:16:44 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+// 1-Write a JavaScript program to calculate the sum of 2 number
+// var num1 = +prompt('enter first number')
+// var num2 = +prompt('enter second number')
+// var sum = num1 + num2;
+// console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+
+
+// Write a JavaScript program to check number if even or odd
+// var num = +prompt("Enter a number")
+// if(num!=0){
+//     if(num%2==0){
+//         console.log("Even");
+//     }
+//     else{
+
+//         console.log("odd");
+
+//     }
+// }
+// else{
+//     console.log("this number is 0");}
+
+// 3- Write a JavaScript program that takes a month and returns the number of days in that month.
+// var month = prompt("Enter the month:");
+// var days;
+
+// switch (month) {
+//   case 'january':
+//   case 'march':
+//   case 'may':
+//   case 'july':
+//   case 'august':
+//   case 'october':
+//   case 'december':
+//    case 'January':
+//   case 'March':
+//   case 'May':
+//   case 'July':
+//   case 'August':
+//   case 'October':
+//   case 'December':
+//     days = 31;
+//     break;
+//   case 'april':
+//   case 'june':
+//   case 'september':
+//   case 'november':
+//     case 'April':
+//   case 'June':
+//   case 'September':
+//   case 'November':
+//     days = 30;
+//     break;
+//   case 'february':
+//     case 'February':
+   
+//     var year = prompt("Enter the year:"); 
+//     year = Number(year); 
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//       days = 29; 
+//     } else {
+//       days = 28; 
+//     }
+//     break;
+//   default:
+//     days = 'Invalid month'; 
+// }
+
+// alert("The number of days in " + month + " is: " + days);
+
+
+
+
+
+// 4- Write a JavaScript program to find max number between 2 number
+//  var num1 = Number(prompt("Enter the first number: "));
+// var num2 = Number(prompt("Enter the second number: "));
+//  if(num1>num2){
+//     console.log(num1 + " is max")
+//  }else{
+//    console.log(num2 + " is max")
+//  }
+
+
+
+
+
+
+// 5- Write  a JavaScript program to check if character vowel or constant
+// var letter = prompt("Enter The  letter")
+// switch (letter) {
+//     case 'a':
+//     case 'o':
+//     case 'e':
+//     case 'i':
+//     case 'u':
+//     case 'A':
+//    case 'O':
+//     case 'E':
+//      case 'I':
+//     case 'U':
+
+//        document.write("character vowel " + "<br>");
+
+//         break;
+//     default:
+//         document.write("character constant " + "<br>");
+
+//  }
